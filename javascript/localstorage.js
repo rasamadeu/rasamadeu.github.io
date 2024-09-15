daddb9e0db541f8377f312f6d6be42b4d319e88e
rasamadeu <rasamadeu1@hotmail.com>
2024-09-15 02:41:30 +0100

    reorganized localstorage js script

---
+++ javascript/localstorage.js
@@ -0,0 +1,25 @@
+// Check if localstorage is available
+function storageAvailable(type) {
+	let storage;
+	try {
+		storage = window[type];
+		const x = "test";
+		storage.setItem(x, x);
+		storage.removeItem(x, x);
+		return true;
+	}
+	catch (e) {
+		return e instanceof DOMexception &&
+			e.name === "QuotaExceededError";
+	}
+}
+
+// Sets localStorage if not defined already
+if (storageAvailable("localStorage") && localStorage.length === 0) {
+	localStorage.setItem("mode", "os");
+	document.querySelector("html").className = "os";
+}
+else {
+	const mode = localStorage.getItem("mode");
+	document.querySelector("html").className = mode;
+}
